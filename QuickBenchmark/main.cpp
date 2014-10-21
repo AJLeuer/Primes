39e3eb0f6422a969039ccef03a2cfb7211c9dfd5
Adam J. Leuer <ajleuer@gmail.com>
2014-10-21 17:34:48 -0500

    Initial Commit

---
+++ QuickBenchmark/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QuickBenchmark
+//
+//  Created by Adam James Leuer on 10/21/14.
+//  Copyright (c) 2014 Adam James Leuer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
